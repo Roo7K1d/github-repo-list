4e344bc081bde1e4d7ae346f34c9c7cda412b718
CancerKid <getfuckedboi@protonmail.com>
2021-06-27 15:08:50 +0200

    for each fix; background

--- index.js
+++ index.js
@@ -1,40 +1,68 @@
 const express = require("express");
 const github = require("github-api");
 const fs = require("fs");
-const trianglify = require('trianglify');
+const readline = require('readline');
+const editJsonFile = require("edit-json-file");
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('./repos.txt'),
+    output: process.stdout,
+    terminal: false
+});
 
 const app = express();
 const port = 3000;
 
 var config = JSON.parse(fs.readFileSync("config.json"));
 
-var git = new github({ token: "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" });
+var git = new github({
+    token: "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"
+});
 
 
 app.set("view engine", "ejs");
 
 app.use(express.static("views"));
 
-var repos = fs.readFileSync('./repos.txt', 'utf8').split('\n');
 
-var canvas = trianglify({
-    width: 1920,
-    height: 1080
-}).toCanvas()
+rl.on('line', async function (line) {
+    var data = line.split(":")
+    var repo = git.getRepo(data[0], data[1])
+    if (!fs.existsSync(`./views/repos/${data[1]}.json`)) {
+        fs.writeFileSync(`./views/repos/${data[1].toString()}.json`, "{}")
+    }
+    let file = editJsonFile(`./views/repos/${data[1]}.json`);
+    await repo.getContributors().then(async function (result) {
+        await file.set("repo_contributors", result.data.length)
+    })
+    await repo.getDetails().then(async function (resultDetails) {
 
-var file = fs.createWriteStream('./views/trianglify.png')
-canvas.createPNGStream().pipe(file)
+        var obj = JSON.parse(fs.readFileSync("./views/lang_colors.json"));
+        var langColor = obj[resultDetails.data.language];
 
+        await file.set("owner_avatar_url", resultDetails.data.owner.avatar_url)
+        await file.set("owner_login", resultDetails.data.owner.login)
+        await file.set("owner_html_url", resultDetails.data.owner.html_url)
+        await file.set("repo_html_url", resultDetails.data.html_url)
+        await file.set("repo_name", resultDetails.data.name)
+        await file.set("repo_open_issues", resultDetails.data.open_issues.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+        await file.set("repo_watchers", resultDetails.data.watchers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+        await file.set("repo_forks", resultDetails.data.forks.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","))
+        await file.set("repo_desc", resultDetails.data.description)
+        await file.set("repo_lang", resultDetails.data.language)
+        await file.set("repo_lang_color", langColor)
+        await file.save()
+    })
+});
 
-app.get("/", (req, res) => { 
+app.get("/", (req, res) => {
 
     res.render("pages/index", {
         config: config,
-        repos: repos,
-        git: git
+        fs: fs
     })
 })
 
 app.listen(port, () => {
-    console.log("Webserver started at https://localhost:" + port);
+    console.log("Webserver started at http://localhost:" + port);
 })
